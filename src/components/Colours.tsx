9ea2888c44adf35e8956f0e2ef9432e3d1d9a143
Matthew Jigalin <mattjigs@gmail.com>
2021-12-12 00:28:03 +1100

    Init typescript colours

---
+++ src/components/Colours.tsx
@@ -0,0 +1,13 @@
+type Colours = {
+  primary: string
+  secondary: string
+  primaryBright: string
+  dark: string
+}
+
+export const ColourPalette: Colours = {
+  primary: "#0DD891",
+  secondary: "#4635b6",
+  primaryBright: "#9fffe1",
+  dark: "#262b3a",
+}
--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -4,6 +4,7 @@ import { PageProps, Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { ColourPalette } from "../components/Colours"
 
 type DataProps = {
   site: {
@@ -14,28 +15,33 @@ type DataProps = {
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <Seo title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
-    <p>
-      This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
-    </p>
-    <p>
-      For type checking you'll want to install <em>typescript</em> via npm and
-      run <em>tsc --init</em> to create a <em>tsconfig</em> file.
-    </p>
-    <p>
-      You're currently on the page "{path}" which was built on{" "}
-      {data.site.buildTime}.
-    </p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/typescript/">
-        documentation about TypeScript
-      </a>
-      .
-    </p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>{ColourPalette.primary}</h1>
+    <div style={{ marginTop: "70px", marginBottom: "70px" }}>
+      <h1 style={{ marginBottom: "50px" }}>
+        Gatsby supports TypeScript by default!
+      </h1>
+      <p>
+        This means that you can create and write <em>.ts/.tsx</em> files for
+        your pages, components etc. Please note that the <em>gatsby-*.js</em>{" "}
+        files (like gatsby-node.js) currently don't support TypeScript yet.
+      </p>
+      <p>
+        For type checking you'll want to install <em>typescript</em> via npm and
+        run <em>tsc --init</em> to create a <em>tsconfig</em> file.
+      </p>
+      <p>
+        You're currently on the page "{path}" which was built on{" "}
+        {data.site.buildTime}.
+      </p>
+      <p>
+        To learn more, head over to our{" "}
+        <a href="https://www.gatsbyjs.com/docs/typescript/">
+          documentation about TypeScript
+        </a>
+        .
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
   </Layout>
 )
 
